8389120297cbea210b33c124555776bc2e90ed8e
Moolana <Moolana@DESKTOP-B5J7CP9>
2023-11-17 16:31:38 +0330

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcService1Client;
+using Google.Protobuf;
+using Grpc.Core;
+
+
+Console.WriteLine("Hello, World!");
+var channel = GrpcChannel.ForAddress("https://localhost:7102");
+
+var client = new Greeter.GreeterClient(channel);
+var result = await client.SayHelloAsync(new HelloRequest { Name = "hamid jalalat" });
+Console.WriteLine(result.Message);
